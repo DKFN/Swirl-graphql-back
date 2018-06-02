b7e7e4e4ab7672a0fe5b79af40585d225006b275
DKFN <valere.tetelin@gmail.com>
2018-06-02 21:51:53 +0000

    modifed prodkick script

--- prodKick.sh
+++ prodKick.sh
@@ -1,5 +1,5 @@
 delOld="rm -rf ./Swirl-graphql-back/swirl-graphql-backend-0.1/"
-playRun="./swirl-graphql-backend-0.1/bin/swirl-graphql-backend -Dplay.http.secret.key=SwirlIsMagic"
+playRun="./swirl-graphql-backend-0.1/bin/swirl-graphql-backend -Dplay.http.secret.key=SwirlIsMagic -Dhttp.port=80"
 
 git pull --rebase
 sbt clean compile dist
