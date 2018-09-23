72fbd095faa909e00b24040056334d000e09559f
DKFN <valere.tetelin@gmail.com>
2018-09-23 15:23:29 +0000

    added -y to kickstart

--- kickstart.sh
+++ kickstart.sh
@@ -1,14 +1,14 @@
 ## Basic dependencies
 sudo apt update
-sudo apt upgrade
-sudo apt install git default-jdk default-jre unzip
+sudo apt -y upgrade
+sudo apt install -y git default-jdk default-jre unzip
 
 ## SBT INSTALL
 
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
 sudo apt-get update
-sudo apt-get install sbt
+sudo apt-get install -y sbt
 
 ## CLONE
 
